aa96ea3240ad4571d1eb9e40a0893528ed5c2442
yotamdvirTAU <yotamdvir@mail.tau.ac.il>
2019-12-05 15:27:01 +0200

    handling NIL and STRING

---
+++ EX2/FOLDER_2_SRC/AST/AST_NIL.java
@@ -0,0 +1,38 @@
+package AST;
+
+public class AST_NIL extends AST_NODE
+{
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_NIL()
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+	
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.format("====================== exp -> NIL\n");
+	}
+
+	/**************************************************/
+	/* The printing message for a simple var AST node */
+	/**************************************************/
+	public void PrintMe()
+	{
+		/**********************************/
+		/* AST NODE TYPE = AST SIMPLE VAR */
+		/**********************************/
+		System.out.format("AST NODE NIL\n");
+
+		/*********************************/
+		/* Print to AST GRAPHIZ DOT file */
+		/*********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("NIL"));
+	}
+}
