67baea96c3e40ebc24c64980932bbe4e8161c09b
yotamdvirTAU <yotamdvir@mail.tau.ac.il>
2019-12-05 14:58:54 +0200

    handling newExp

---
+++ EX2/FOLDER_2_SRC/AST/AST_EXP_NEW_EXP.java
@@ -0,0 +1,58 @@
+package AST;
+
+public class AST_EXP_NEW_EXP extends AST_NODE
+{
+	public AST_EXP e;
+	public String fieldName;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_EXP_NEW_EXP(String name, AST_EXP e)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.format("====================== newExp -> NEW ID( %s ) [ exp ]\n",name);
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.var = var;
+		this.fieldName = fieldName;
+	}
+
+	/*************************************************/
+	/* The printing message for a field var AST node */
+	/*************************************************/
+	public void PrintMe()
+	{
+		/*********************************/
+		/* AST NODE TYPE = AST FIELD VAR */
+		/*********************************/
+		System.out.print("AST NODE EXP NEW EXP\n");
+
+		/**********************************************/
+		/* RECURSIVELY PRINT VAR, then FIELD NAME ... */
+		/**********************************************/
+		e.PrintMe();
+		System.out.format("ID( %s )\n",name);
+
+		/***************************************/
+		/* PRINT Node to AST GRAPHVIZ DOT file */
+		/***************************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("EXP\nNEW_EXP\n...->%s",name));
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,e.SerialNumber);
+	}
+}
