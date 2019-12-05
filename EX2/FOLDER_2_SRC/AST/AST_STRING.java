fb3625382033e019dac2eef02881b817f6401c57
yotamdvirTAU <yotamdvir@mail.tau.ac.il>
2019-12-05 15:27:01 +0200

    handling NIL and STRING

---
+++ EX2/FOLDER_2_SRC/AST/AST_STRING.java
@@ -0,0 +1,48 @@
+package AST;
+
+public class AST_STRING extends AST_NODE
+{
+	/************************/
+	/* simple variable str */
+	/************************/
+	public String str;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_STRING(String str)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+	
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.format("====================== exp -> STRING( %s )\n",str);
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.str = str;
+	}
+
+	/**************************************************/
+	/* The printing message for a simple var AST node */
+	/**************************************************/
+	public void PrintMe()
+	{
+		/**********************************/
+		/* AST NODE TYPE = AST SIMPLE VAR */
+		/**********************************/
+		System.out.format("AST NODE STRING( %s )\n",str);
+
+		/*********************************/
+		/* Print to AST GRAPHIZ DOT file */
+		/*********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("STRING\n(%s)",str));
+	}
+}
