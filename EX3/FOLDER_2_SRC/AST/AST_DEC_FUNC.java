cee0520da6b3bac87ba200985fc008e2179e7df3
liwoxa <>
2020-01-09 18:43:37 +0200

    e

--- EX3/FOLDER_2_SRC/AST/AST_DEC_FUNC.java
+++ EX3/FOLDER_2_SRC/AST/AST_DEC_FUNC.java
@@ -116,10 +116,11 @@ public class AST_DEC_FUNC extends AST_DEC
 		TYPE returnType = null;
 		if (returnTypeName == "void") {
 		    returnType = TYPE_VOID.getInstance();
+		    debug_print("if: " + returnType);
 		} else {
 		    returnType = sym_table.find(returnTypeName);
+		    debug_print("else: " + returnType);
 		}
-		debug_print("returnTypeKind: " + returnType.getKind());
 		if (returnType == null || (!returnType.isTypeName() && !returnType.isVoid()))
 		{
 		    report_error("// Code bug -- type to return does not exist in table or just is not a name a of a type nor void");
