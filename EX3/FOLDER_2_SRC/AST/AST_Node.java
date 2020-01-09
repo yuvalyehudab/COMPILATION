0c9c77561067bcb0ebbf1960724b9ada80521d39
liwoxa <>
2020-01-09 17:48:26 +0200

    static rrr

--- EX3/FOLDER_2_SRC/AST/AST_Node.java
+++ EX3/FOLDER_2_SRC/AST/AST_Node.java
@@ -2,7 +2,7 @@ package AST;
 
 import java.io.*;
 
-public abstract class AST_Node
+public class AST_Node
 {
 	/*******************************************/
 	/* The serial number is for debug purposes */
